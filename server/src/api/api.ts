750343869d37bd34ddcc146ab36a73a2362b31ca
Abhishek8841 <abhishekbatra2005@gmail.com>
2026-06-09 20:12:27 +0530

    minor

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -1,21 +1,22 @@
-
+import type { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import express from "express";
 import { router1 } from "./routes/auth.routes.js";
-import { router2 } from "./routes/conversation.js";
+import { router2 } from "./routes/conversation.router.js";
 
 const app = express();
+
 app.use(cookieParser());
 app.use(express.json());
 
-app.use((_, res) => {
-    res.status(404).json({
-        success: false,
-        message: "Route not found"
-    });
-});
-
 app.use("/api/v1", router1);
 app.use("/api/v1", router2);
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        message: `Cannot ${req.method} ${req.originalUrl}`
+    });
+});
 
 export default app;
