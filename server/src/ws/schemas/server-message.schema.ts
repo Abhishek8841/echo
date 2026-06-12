6cae7ef91f660f0a9592a5e0f741815ee5b72b50
Abhishek8841 <abhishekbatra2005@gmail.com>
2026-06-12 14:42:26 +0530

    minor

--- server/src/ws/schemas/server-message.schema.ts
+++ server/src/ws/schemas/server-message.schema.ts
@@ -12,16 +12,3 @@ export const serverMessageSchema = z.object({
 })
 
 export type serverMessageType = z.infer<typeof serverMessageSchema>;
-
-
-// (alias) type MessagesType = {
-//     id: string;
-//     content: string;
-//     senderId: string;
-//     receiverId: string;
-//     createdAt: Date;
-//     sender: {
-//         id: string;
-//         email: string;
-//     };
-// }[]
