57282c120e34e42951144ce59ae1f2a354b7bc1d
Abhishek8841 <abhishekbatra2005@gmail.com>
2026-06-14 22:00:15 +0530

    minor

--- server/src/api/routes/conversation.router.ts
+++ server/src/api/routes/conversation.router.ts
@@ -5,5 +5,6 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 export const router2 = express.Router();
 
 router2.get("/users", authMiddleware, getUserList);
+// remember to keep static routes above dynamic !_!
 router2.get("/messages/unreadCount", authMiddleware, getUnreadCount);
 router2.get("/messages/:id", authMiddleware, getMessageList);
